27b3aaf5ad4e6a095671ec6f2fa394271c28b1dc
agustin-f <farisanoagustin@gmail.com>
2026-01-23 23:17:59 -0300

    agrego rutas, banner en home y componente para texto de bienvenida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { CartPage } from './pages/cart-page/cart-page';
 import { ProductCard } from './components/product-card/product-card';
+import { BannerHome } from './components/banner-home/banner-home';
+import { CartPage } from './pages/cart-page/cart-page';
+import { CartSummary } from './components/cart-summary/cart-summary';
 
 export const routes: Routes = [
-  { path: '/', component: ProductCard },
-  { path: 'productos', component: ProductCard },
-  { path: '**', redirectTo: 'productos' },
+  { path: '', component: CartPage },
+  { path: 'productos', component: CartSummary },
+  { path: '**', redirectTo: '' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { CartPage } from './pages/cart-page/cart-page';
 import { Chatinputs } from './components/chatinputs/chatinputs';
+import { BannerHome } from "./components/banner-home/banner-home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CartPage, Navbar, Chatinputs],
+  imports: [RouterOutlet, CartPage, Navbar, Chatinputs, BannerHome],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
